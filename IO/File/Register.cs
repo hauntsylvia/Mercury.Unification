523ee3dd7979fac320a99913adc249e92e25fc59
Izabella Williams <iza.yy10@gmail.com>
2022-03-09 15:54:15 -0500

    a

--- IO/File/Register.cs
+++ IO/File/Register.cs
@@ -29,23 +29,39 @@ namespace Mercury.Unification.IO.File
 
         public Record<T>? GetRecord<T>(string Key)
         {
-            FileInfo FileInfo = this.GetFileInfoFromKey(Key);
-            if (FileInfo.Exists)
+            try
             {
-                using StreamReader Reader = new(FileInfo.OpenRead());
-                Record<T>? Attempt = JsonConvert.DeserializeObject<Record<T>>(Reader.ReadToEnd());
-                return Attempt;
+                FileInfo FileInfo = this.GetFileInfoFromKey(Key);
+                if (FileInfo.Exists)
+                {
+                    using StreamReader Reader = new(FileInfo.OpenRead());
+                    Record<T>? Attempt = JsonConvert.DeserializeObject<Record<T>>(Reader.ReadToEnd());
+                    return Attempt;
+                }
+                else
+                    return null;
             }
-            else
-                return null;
+            catch(Exception E)
+            {
+                Console.WriteLine(E);
+                throw;
+            }
         }
 
 
         public void SaveRecord<T>(string Key, Record<T> Record)
         {
-            FileInfo FileInfo = this.GetFileInfoFromKey(Key);
-            using StreamWriter Writer = new(FileInfo.FullName);
-            Writer.Write(JsonConvert.SerializeObject(Record));
+            try
+            {
+                FileInfo FileInfo = this.GetFileInfoFromKey(Key);
+                using StreamWriter Writer = new(FileInfo.FullName);
+                Writer.Write(JsonConvert.SerializeObject(Record, Formatting.Indented));
+            }
+            catch(Exception E)
+            {
+                Console.WriteLine(E);
+                throw;
+            }
         }
     }
 }
