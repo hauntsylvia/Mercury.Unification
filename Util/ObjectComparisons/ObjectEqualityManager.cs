439d168fa0f215d94723f0083a3699e0a4e50440
Izabella Williams <iza.yy10@gmail.com>
2022-03-28 15:12:44 -0400

    internals change

---
+++ Util/ObjectComparisons/ObjectEqualityManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercury.Unification.Util.ObjectComparisons
+{
+    public static class ObjectEqualityManager
+    {
+        public static bool PropertiesAreEqual(object A, object B)
+        {
+            if(A != null && B != null)
+            {
+                foreach (PropertyInfo AInfo in A.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    foreach (PropertyInfo BInfo in B.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                    {
+                        if (AInfo.Name == BInfo.Name && AInfo.PropertyType == BInfo.PropertyType && AInfo.Name != "Id")
+                        {
+                            object? AValue = AInfo.GetValue(A, null);
+                            object? BValue = BInfo.GetValue(B, null);
+                            if (AValue != null && BValue != null && !AValue.Equals(BValue) && !(AValue == null && BValue == null))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
